56ae343550d74b12ae6f1df02435003257066a5b
Alex Ramos <estefano16@hotmail.es>
2023-11-08 15:45:49 -0500

    commit inicial

---
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_aero/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.b2023_gr1sw_aero
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
