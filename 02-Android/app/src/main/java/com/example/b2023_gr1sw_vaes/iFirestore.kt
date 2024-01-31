0cc9dde1d51a4eb05c38167e15c4d9e3064678d5
Alex Ramos <estefano16@hotmail.es>
2024-01-30 20:08:20 -0500

    30/01/2024

---
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_vaes/iFirestore.kt
@@ -0,0 +1,11 @@
+package com.example.b2023_gr1sw_vaes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class iFirestore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ifirestore)
+    }
+}
