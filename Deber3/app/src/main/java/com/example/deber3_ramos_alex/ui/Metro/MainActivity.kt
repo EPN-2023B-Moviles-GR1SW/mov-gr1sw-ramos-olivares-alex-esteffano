233dbde1ee37585f41e288af5f31f2f23dbb471c
Alex Ramos <estefano16@hotmail.es>
2024-03-02 17:41:01 -0500

    deber 3

---
+++ Deber3/app/src/main/java/com/example/deber3_ramos_alex/ui/Metro/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.deber3_ramos_alex.ui.Metro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deber3_ramos_alex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
